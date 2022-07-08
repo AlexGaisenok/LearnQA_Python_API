7ea14bfdb0f21814a8707ae3294d3fb147454434
AlexGaisenok <43135603+AlexGaisenok@users.noreply.github.com>
2022-07-08 20:15:51 +0700

    Add files via upload

---
+++ test_userAgent.py
@@ -0,0 +1,48 @@
+import pytest
+import requests
+import json
+class Test_UserAgent:
+    headers = [
+            ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'),
+            ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1'),
+             ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'),
+             ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'),
+             ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1')
+        ]
+    @pytest.mark.parametrize('header',headers)
+    def test_userAgent_method(self, header):
+        
+        platforms = [
+        ('Mobile'),
+        ('Mobile'),
+        ('Googlebot'),
+        ('Web'),
+        ('Mobile')
+        ]
+        browsers = [
+        ('No'),
+        ('Chrome'),
+        ('Unknown'),
+        ('Chrome'),
+        ('No')
+        ]
+        devices = [
+            ('Android'),
+            ('iOS'),
+            ('Unknown'),
+            ('No'),
+            ('iPhone')
+        ]
+        response = requests.get('https://playground.learnqa.ru/ajax/api/user_agent_check',headers={"User-Agent": header})
+        user_agent_Data = response.json()
+        for i in range(len(platforms)):
+            # print('***********************************************************')
+            # print(user_agent_Data['platform'],'--------------', platforms[i])
+            # print(user_agent_Data['browser'],'--------------', browsers[i])
+            # print(user_agent_Data['device'],'--------------',devices[i])
+            # print('***********************************************************')
+            assert user_agent_Data['platform']==platforms[i], 'Wrong value of a parameter'
+            assert user_agent_Data['browser']==browsers[i], 'Wrong value of a parameter'
+            assert user_agent_Data['device']==devices[i], 'Wrong value of a parameter'
+            break
+            
